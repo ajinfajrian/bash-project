0b97ea95e9ca147917aa7c81fa295e856d9de104
fajrian <fajrian.ajin@gmail.com>
2022-07-06 19:49:10 +0700

    adding installation k8s

--- container-inst.sh
+++ container-inst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Guide Installation by Docker Docs"
+echo "Install Docker and K8s Apps"
 sudo apt update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
@@ -9,4 +9,10 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo usermod -aG docker $USER
+printf "=== Installation K8s  ==="
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/kubernetes-xenial main"
+sudo apt-get install kubeadm kubelet kubectl
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff -a
 echo "Finish Installation"
